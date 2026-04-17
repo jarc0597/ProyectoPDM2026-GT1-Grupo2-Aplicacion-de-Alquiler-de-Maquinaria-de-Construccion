8e43c6b13d33519aebb88933ea2da89317f4fc4b
Josué Rivera <rc18025@ues.edu.sv>
2026-04-17 09:35:36 -0600

    Cargo Inicial del Proyecto -pantalla de mainActivity (login) -Panralla de registro

---
+++ app/src/main/java/com/proyecto/pdm115/alquilermaquinaria/RegisterActivity.kt
@@ -0,0 +1,25 @@
+package com.proyecto.pdm115.alquilermaquinaria
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnBack = findViewById<ImageButton>(R.id.btn_back)
+        btnBack.setOnClickListener {
+            finish() // Esto cierra la pantalla actual y vuelve a la anterior
+        }
+    }
+}
